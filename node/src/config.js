ef0d4f90a6a94fa7c8f49806c2b07bd6f27775df
MaJun <1422205209@qq.com>
2020-05-11 22:39:43 +0800

    修改配置

--- node/src/config.js
+++ node/src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   "token": "mytest",
-  "appId": "wx75493745291de9ed", // 测试账号
-  "appSecret": "0c9ed65b4f0643792f079bc54728420a" // 测试账号
+  "appId": "wxa53d44a3453298f2", // 测试账号
+  "appSecret": "a1969694c511db3f4fb2bedc8b08d712" // 测试账号
 }
